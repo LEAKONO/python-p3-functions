bed4b014b6024f4d2e227673407e924babaa60ef
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-05-20 12:59:38 +0300

    functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+    return "Hello, programmer!"
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+    return f"Hello, {name}!"
+greet("Alice")
+
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+    return f"Hello, {name}!"
+greet_with_default("Alice")
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    return num1 + num2
+add(3,4)
 
 def halve(number):
-    pass
+    print(number / 2)
+    return number / 2
+halve(10)
